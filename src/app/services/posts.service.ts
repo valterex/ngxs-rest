bf716c93f768fb37a5f2ad29eca61cc8082cc6f0
Valter <vcvijic@protonmail.com>
2021-08-03 00:08:37 +0200

    finalize crud operations

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
-import { Post } from '../models/Post';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { NewPost, PatchedPost, Post } from '../models/Post';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -12,9 +12,54 @@ export class PostsService {
   apiUrl = environment.apiUrl;
   postsUrl = '/posts';
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
   constructor(private http: HttpClient) {}
 
   getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>(this.apiUrl + this.postsUrl);
+    return this.http
+      .get<Post[]>(this.apiUrl + this.postsUrl)
+      .pipe(map((res) => res.splice(0, 10)));
+  }
+
+  getPost(postId: number): Observable<Post> {
+    return this.http.get<Post>(this.apiUrl + this.postsUrl + `/${postId}`);
+  }
+
+  getPostComments(postId: number): Observable<Comment[]> {
+    return this.http.get<Comment[]>(
+      this.apiUrl + this.postsUrl + `/${postId}/comments`
+    );
+  }
+
+  getPostsByUser(userId: number): Observable<Post[]> {
+    return this.http.get<Post[]>(
+      this.apiUrl + this.postsUrl + `?userId=${userId}`
+    );
+  }
+
+  createPost(data: NewPost): Observable<Post> {
+    return this.http.post<Post>(
+      this.apiUrl + this.postsUrl,
+      data,
+      this.httpOptions
+    );
+  }
+
+  deletePost(postId: number): Observable<Post> {
+    return this.http.delete<Post>(
+      this.apiUrl + this.postsUrl + `/${postId}`,
+      this.httpOptions
+    );
+  }
+
+  patchPost(data: PatchedPost, postId: number): Observable<Post> {
+    return this.http.patch<Post>(
+      this.apiUrl + this.postsUrl + `/${postId}`,
+      data,
+      this.httpOptions
+    );
   }
 }
